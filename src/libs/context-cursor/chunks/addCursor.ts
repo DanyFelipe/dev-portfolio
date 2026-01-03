383d6cd5e1caf72f0bd3f650cef0d90cbe93c4c3
DanyFelipe <danyfelipe117@gmail.com>
2026-01-03 00:53:57 -0500

    Bug fix

--- src/libs/context-cursor/chunks/addCursor.ts
+++ src/libs/context-cursor/chunks/addCursor.ts
@@ -1,14 +1,15 @@
-const addCursor = (props: CProps) => {
-  if (props) {
-    const contextCursor = document.createElement("div");
-    contextCursor.setAttribute(
-      "style",
-      `transform: translate(-200px, -200px); height: ${props.radius}px; width: ${props.radius}px;`
-    );
-    contextCursor.setAttribute("class", "c-cursor");
-    document.body.prepend(contextCursor);
-    return contextCursor;
-  }
+const addCursor = (props: CProps): HTMLDivElement | null => {
+  if (!props) return null;
+
+  const contextCursor = document.createElement("div");
+  contextCursor.setAttribute(
+    "style",
+    `transform: translate(-200px, -200px); height: ${props.radius}px; width: ${props.radius}px;`
+  );
+  contextCursor.setAttribute("class", "c-cursor");
+  document.body.prepend(contextCursor);
+
+  return contextCursor;
 };
 
 export default addCursor;
